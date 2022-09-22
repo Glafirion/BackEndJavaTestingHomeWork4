acbd2a68a389be02391e15a201178e57a3d3fd71
Vadim 5A <mooggle@rambler.ru>
2022-09-22 15:37:54 +0300

    HW4 hardwork

---
+++ src/main/java/com/geekbrains/api/SpoonaccularService.java
@@ -0,0 +1,33 @@
+package com.geekbrains.api;
+
+import retrofit2.Call;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.Body;
+
+public class SpoonaccularService {
+
+    private SpoonaccularApi api;
+    private static final String API_KEY = "c536485c7c8f44229fba81ea7b8bcd60";
+
+    public SpoonaccularService() {
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://api.spoonacular.com/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        api = retrofit.create(SpoonaccularApi.class);
+    }
+
+    public ApiSearchResult findRecipes(String query, Integer number) {
+        Call<ApiSearchResult> call = api.findRecipes(API_KEY, query, number);
+        return RetrofitUtils.execute(call);
+    }
+
+    public ApiUserConnectResult connect(@Body ApiUserConnectRequest request) {
+        Call<ApiUserConnectResult> call = api.connect(request, API_KEY);
+        return RetrofitUtils.execute(call);
+    }
+
+}
